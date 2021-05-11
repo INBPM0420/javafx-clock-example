19b93b6358654ee63f05fbec92d3b8e2c8a87e81
Péter Jeszenszky <jeszenszky.peter.deik@gmail.com>
2021-05-11 13:38:51 +0200

    Initial import

---
+++ src/main/java/clock/ClockApplication.java
@@ -0,0 +1,22 @@
+package clock;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ClockApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(ClockApplication.class.getResource("/clock.fxml"));
+        stage.setTitle("JavaFX Clock");
+        stage.setResizable(false);
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+}
